b9aafd191133a9560e7d3c4e21098b71ee676827
eimuc <43643903+eimuc@users.noreply.github.com>
2020-10-07 15:39:35 +0300

    css update

---
+++ js/data.js
@@ -0,0 +1,97 @@
+const dashboard = [
+    {
+        icon: 'fa fa-facebook-official',
+        nick: '@nathanf',
+        number: 1987,
+        followers: 'F o l l o w e r s',
+        arrow: 'fa fa-caret-up',
+        today: '12 Today'
+    },
+    {
+        icon: 'fa fa-twitter',
+        nick: '@nathanf',
+        number: 1044,
+        followers: 'F o l l o w e r s',
+        arrow: 'fa fa-caret-up',
+        today: '99 Today'
+    },
+    {
+        icon: 'fa fa-instagram',
+        nick: '@@realnathanf',
+        number: '11k',
+        followers: 'F o l l o w e r s',
+        arrow: 'fa fa-caret-up',
+        today: '1099 Today'
+    },
+    {
+        icon: 'fa fa-youtube-play',
+        nick: 'Nathan F.',
+        number: 8239,
+        followers: 'S U B S C R I B E R S',
+        arrow: 'fa fa-caret-down',
+        today: '144 Today'
+    },
+]
+
+const stats = [
+    {
+        title: 'Page Views',
+        icon: 'fa fa-facebook-official',
+        number: 87,
+        arrow: 'fa fa-caret-up',
+        percentage: '3%'
+    },
+    {
+        title: 'Likes',
+        icon: 'fa fa-facebook-official',
+        number: 52,
+        arrow: 'fa fa-caret-down',
+        percentage: '2%'
+    },
+    {
+        title: 'Likes',
+        icon: 'fa fa-instagram',
+        number: 5462,
+        arrow: 'fa fa-caret-up',
+        percentage: '2257%'
+    },
+    {
+        title: 'Profile Views',
+        icon: 'fa fa-instagram',
+        number: '52k',
+        arrow: 'fa fa-caret-up',
+        percentage: '1375%'
+    },
+    {
+        title: 'Retweets',
+        icon: 'fa fa-twitter',
+        number: 117,
+        arrow: 'fa fa-caret-up',
+        percentage: '303%'
+    },
+    {
+        title: 'Likes',
+        icon: 'fa fa-twitter',
+        number: 507,
+        arrow: 'fa fa-caret-up',
+        percentage: '553%'
+    },
+    {
+        title: 'Likes',
+        icon: 'fa fa-youtube-play',
+        number: 107,
+        arrow: 'fa fa-caret-down',
+        percentage: '19%'
+    },
+    {
+        title: 'Total Views',
+        icon: 'fa fa-youtube-play',
+        number: 1407,
+        arrow: 'fa fa-caret-down',
+        percentage: '12%'
+    },
+]
+
+
+const data = {dashboard, stats};
+export default data;
